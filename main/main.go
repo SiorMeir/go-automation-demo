2b9ebb026c7b98f55353af4add19bf86302f0db1
SiorMeir <msior@nvidia.com>
2025-09-15 15:28:34 +0300

    feat: add endpoint

--- main/main.go
+++ main/main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/goodbye", goodbyeHandler)
+	http.HandleFunc("/gdsgs", gdsgs)
 
 	fmt.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -22,6 +23,11 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `{"message": "bye bye, World!"}`)
+}
+func gdsgs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, `{"message": "bye bye, World!"}`)
