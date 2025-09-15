07030af3c42897b13e22595cb16241bfe743f945
Sior Meir <121446396+SiorMeir@users.noreply.github.com>
2025-09-15 15:29:12 +0300

    Revert "feat: add endpoint"

--- main/main.go
+++ main/main.go
@@ -10,7 +10,6 @@ func main() {
 	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/health", healthHandler)
 	http.HandleFunc("/goodbye", goodbyeHandler)
-	http.HandleFunc("/gdsgs", gdsgs)
 
 	fmt.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -23,11 +22,6 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, `{"message": "bye bye, World!"}`)
-}
-func gdsgs(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, `{"message": "bye bye, World!"}`)
